6922b6467624a30d372419613864ddf79041aa74
Developer Experience team at Ignite <hello@ignite.com>
2026-09-07 16:43:43 +0300

    Initialized with Ignite CLI

---
+++ cmd/feracd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/serhiibudianskyi/ferac/app"
+	"github.com/serhiibudianskyi/ferac/cmd/feracd/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
